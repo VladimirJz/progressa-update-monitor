6d852ba387af7e60e85e08cb6af498531215300d
=Vladimir <=vladimir.jz@hotmail.com>
2023-04-24 17:01:21 -0600

    changes

--- update_monitor.py
+++ update_monitor.py
@@ -9,18 +9,43 @@ import datetime
 
 
 def main():
-    is_available=False
-    CONFIG_FILE='/opt/progressa/srv/update-monitor/safi.cfg'
-    start_job = datetime.datetime.now()
-    service_config=Utils.load_settings(CONFIG_FILE,section='SERVICE')
-    log_config=Utils.load_settings(CONFIG_FILE,section='LOG')
-    debug=False
-    if log_config['servicelogtrace']==1:
-        debug=True
 
-    endpoint=service_config.get('updatebalanceendpoint')
-    settings=Utils.load_settings(CONFIG_FILE)        
-    logger=Utils.log_handler('/opt/progressa/logs/update-monitor.log')
+    def save_request(args,end_point=None,success=None):
+        OBJETCTYPE=3
+        SERVICEID= 3
+        if success:
+            args['accepted_at']=datetime.datetime.now()
+        
+        args['date']=datetime.date.today()
+        args['object_type']=OBJETCTYPE
+        args['service_id']=SERVICEID
+        #args['end_point']=end_point
+        args['attempts']=1
+        
+
+
+        print(args)
+        args=Utils.to_json([args])
+        print(args[0])
+        r=Utils.post(args[0],end_point)
+        return r
+
+    #----- Archivo de configuración
+    CONFIG_FILE='/opt/progressa/srv/update-monitor/safi.cfg'
+    
+    service_settings=Utils.load_settings(CONFIG_FILE,section='SERVICE')
+    log_settings=Utils.load_settings(CONFIG_FILE,section='LOG')
+    settings=Utils.load_settings(CONFIG_FILE)
+    save_resposeWS=service_settings.get('saveresponseendpoint',None)
+    endpoint=service_settings.get('updatebalanceendpoint')
+    log_file=log_settings.get('logfile','')
+    logger=Utils.log_handler(log_file)        
+
+    is_available=False
+    start_job = datetime.datetime.now()
+    debug=False
+    if log_settings['servicelogtrace']==1:
+        debug=True
 
     try :
         db=Connector(**settings)
@@ -30,45 +55,50 @@ def main():
 
     if is_available :
         logger.info('Conexión a la BD exitosa.')
-        #saldos_actualizados=Request.Integracion(PGSS_SALDOSDIARIOS).add(Tipo='I',Instrumento='C',OrigenID=111111130)
         saldos_actualizados=Request.Integracion(PGSS_SALDOSDIARIOS).add(Tipo='I')
-        #saldos_actualizados=Request.Integracion(PGSS_SALDOSDIARIOS).add(Tipo='I',Instrumento='C',OrigenID=11130)
-
-        logger.info(saldos_actualizados.routine)
-        logger.info(saldos_actualizados.parameters)
         try:
-         data=db.get(saldos_actualizados)
+            results=db.get(saldos_actualizados)
         except Exception:
-         logger.critical('Error al ejecutar rutina ',exc_info=True)
-         print(data.data)
-        if(data.status_code==0):
+            logger.critical('Error al ejecutar rutina ',exc_info=True)
+            
+        if(results.status_code==0):
             logger.info(f'Información generada correctamente, registros  por procesar:{data.rowcount }')
-            if(data.rowcount>0):
-                json=data.to_json()
+            if(results.rowcount>0):
+                json_data=results.to_json(key='PrestamoId')
                 if not Utils.is_reachable(endpoint): 
                     logger.info(f'El EndPoint esta disponible: { endpoint }')
                     success=0
                     error=0
                     logger.info(f'Comienza consumo del EndPoint.')
-                    for resultados in json:
+                    for json_string in json_data:
+                        response={}
+             
                         try :
-                            r=Utils.post(resultados,end_point=endpoint)
+                            r=Utils.post(json_string,end_point=endpoint)
                             if(debug):
                                 logger.info(f'Petición exitosa, Endpoint Response: {r.status_code} -{r.reason}  -  elapsed at: { r.elapsed }')
                             if(not r.ok):
                                 error=error+1
                                 logger.warning(f'Petición rechazada, Response: {r.status_code} -{r.reason}  -  elapsed at: { r.elapsed }')
                                 logger.warning(f'Respuesta: {r.text}')
-                                logger.warning('= payload =========>>\n' + resultados)
+                                logger.warning('= payload =========>>\n' + json_string)
                                 logger.warning('<<===========payload=')
                             else:
                                 success=success+1
-
                             
 
                         except Exception as e:
                             logger.critical('Error al procesar la peticion POST.' , exc_info=True)
                             logger.critical(e)
+                        
+                        else:
+                            response['status_code']=r.status_code
+                            response['reason']=r.reason
+                            response['message']=r.text
+                            
+                        response['object_key']= int(PrestamoId)
+                        response['original_request']=str(json_string)
+                        response['end_point']='http://10.90.0.71:28108/api/v1/ProductosProgressa/insert'
 
                             return False
                     end_job = datetime.datetime.now()
